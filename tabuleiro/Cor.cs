86ec2549e2f20391f14dd675b9404c9f79274007
Rodrigo Vieira <rodrigovf21021994@gmail.com>
2022-02-08 15:47:21 -0300

    Adicionando classe tabuleiro, peca, posicao e cor

---
+++ tabuleiro/Cor.cs
@@ -0,0 +1,8 @@
+﻿namespace tabuleiro
+{
+    public enum Cor
+    {
+        Branca,
+        Preta
+    }
+}
---
+++ tabuleiro/Tabuleiro.cs
@@ -0,0 +1,17 @@
+﻿namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Pecas[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            pecas = new Pecas[linhas, colunas];
+        }
+
+    }
+}
